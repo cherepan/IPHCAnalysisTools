855e52d620736ef3dda9d747cba9a5a0ca3649d9
Vladimir <vladimir.cherepanov@cern.ch>
2018-06-21 15:54:30 +0200

    junk

--- TauSpinerInterface.cxx
+++ TauSpinerInterface.cxx
@@ -78,29 +78,32 @@ double TauSpinerInterface::Get(int type, SimpleParticle X, SimpleParticle tau, s
     std::cout << "Tau2reco " << Tau2.Px() << " " << Tau2.Py() << " " << Tau2.Pz() << " " << Tau2.E() << " " << Tau2.M() << std::endl;
     */
 
+   // TauSpinner::SimpleParticle boson(9.057124, 4.738310, 362.649353, 373.200989, 23);
+    
+    // TauSpinner::SimpleParticle tau1(-7.145843, 32.856289, 59.924850, 68.736778, -15);
+    
+    // std::vector<TauSpinner::SimpleParticle> tauFinalStates1;
+    // tauFinalStates1.push_back(TauSpinner::SimpleParticle(-0.471688, 3.796974, 7.760280, 8.652251, -16)); // neutrino
+    // tauFinalStates1.push_back(TauSpinner::SimpleParticle(-6.674153, 29.059315, 52.164566, 60.084522, 211)); // pion
+    
+    // TauSpinner::SimpleParticle tau2(16.202969, -28.117977, 302.724579, 304.464264, 15);
+    
+    // std::vector<TauSpinner::SimpleParticle> tauFinalStates2;
+    // tauFinalStates2.push_back(TauSpinner::SimpleParticle(13.371717, -21.770473, 237.549759, 238.919739, 16)); // neutrino
+    // tauFinalStates2.push_back(TauSpinner::SimpleParticle(2.831248, -6.347505, 65.174820, 65.544518, -211)); // pion
+
+
+
+
 
 
     int eventNumber = EventNumber;
     std::cout << "\nevent = " << EventNumber << std::endl;
     
-    TauSpinner::SimpleParticle boson(9.057124, 4.738310, 362.649353, 373.200989, 23);
-    
-    TauSpinner::SimpleParticle tau1(-7.145843, 32.856289, 59.924850, 68.736778, -15);
-    
-    std::vector<TauSpinner::SimpleParticle> tauFinalStates1;
-    tauFinalStates1.push_back(TauSpinner::SimpleParticle(-0.471688, 3.796974, 7.760280, 8.652251, -16)); // neutrino
-    tauFinalStates1.push_back(TauSpinner::SimpleParticle(-6.674153, 29.059315, 52.164566, 60.084522, 211)); // pion
-    
-    TauSpinner::SimpleParticle tau2(16.202969, -28.117977, 302.724579, 304.464264, 15);
-    
-    std::vector<TauSpinner::SimpleParticle> tauFinalStates2;
-    tauFinalStates2.push_back(TauSpinner::SimpleParticle(13.371717, -21.770473, 237.549759, 238.919739, 16)); // neutrino
-    tauFinalStates2.push_back(TauSpinner::SimpleParticle(2.831248, -6.347505, 65.174820, 65.544518, -211)); // pion
-
-
-    srand(391486);
-    //    WT = calculateWeightFromParticlesH(X, tau, tau2, tau_daughters,tau_daughters2);
-    WT = calculateWeightFromParticlesH(boson, tau1, tau2, tauFinalStates1,tauFinalStates2);
+ 
+    srand(eventNumber);
+    WT = calculateWeightFromParticlesH(X, tau, tau2, tau_daughters,tau_daughters2);
+    //  WT = calculateWeightFromParticlesH(boson, tau1, tau2, tauFinalStates1,tauFinalStates2);
     double polSM=getTauSpin();
     std::cout<<"  TauSPinner Interface " <<std::endl;
     std::cout<<"  getTauSpin():   "<< polSM <<" WT:   "<<WT<<std::endl;
